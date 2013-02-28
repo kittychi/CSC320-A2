4c2b147c9a57b5cbde56ce39db3ec194c5567886
Christy Sham <g2g@cdf.toronto.edu>
2013-02-27 23:56:15 -0500

    changed lookup to use psi, needs work

--- partA/src/inpainting/patch_db.cxx
+++ partA/src/inpainting/patch_db.cxx
@@ -10,60 +10,60 @@
 #include "psi.h"
 
 patch_db::patch_db(
-			const vil_image_view<vil_rgb<vxl_byte> >& im, 
-			vil_image_view<bool> unfilled, int patch_radius
-			)
+  const vil_image_view<vil_rgb<vxl_byte> >& im, 
+  vil_image_view<bool> unfilled, int patch_radius
+)
 
-	// we initialize the size of the patch_center_coords_
-	// matrix to the larged possible number of completely
-	// full patches in the image
-	: patch_center_coords_(im.ni() * im.nj(), 2)
+// we initialize the size of the patch_center_coords_
+// matrix to the larged possible number of completely
+// full patches in the image
+: patch_center_coords_(im.ni() * im.nj(), 2)
 {
-	rows_ = 0;
-	w_ = patch_radius;
-	plen_ = (2*w_ + 1) * (2*w_ + 1);
-	nplanes_ = 3;
-
-	// create a local copy of the source image
-	vil_copy_deep(im, im_);
-
-	// compute the matrix of centers of the patches 
-	// that are completely full (ie. contain no unfilled
-	// pixels)
-	compute_patch_centers(im, unfilled);
+  rows_ = 0;
+  w_ = patch_radius;
+  plen_ = (2*w_ + 1) * (2*w_ + 1);
+  nplanes_ = 3;
+  
+  // create a local copy of the source image
+  vil_copy_deep(im, im_);
+  
+  // compute the matrix of centers of the patches 
+  // that are completely full (ie. contain no unfilled
+  // pixels)
+  compute_patch_centers(im, unfilled);
 }
 
 
 void patch_db::compute_patch_centers(
-			const vil_image_view<vil_rgb<vxl_byte> >& im,
-			vil_image_view<bool> uf
-			)
+  const vil_image_view<vil_rgb<vxl_byte> >& im,
+  vil_image_view<bool> uf
+)
 {
-	int plane;
-	int i, j;
-	bool full;
-	int pi, pj;
-	int vi;
-
-	// loop over all possible patch centers
-	top_ = 0;
-	for (i=w_; i<im.ni()-w_; i++)
-		for (j=w_; j<im.nj()-w_; j++) {
-			full = true;
-			patch_center_coords_(top_, 0) = i;
-			patch_center_coords_(top_, 1) = j;
-			// loop over all pixels in a patch
-			for (pi=-w_, vi=0; (pi<=w_) && full; pi++)
-				for (pj=-w_; (pj<=w_) && full; pj++, vi++) 
-					// if pixel is unfilled, the patch is
-					// not completely full so this patch
-					// cannot be used for lookup operations
-					if (uf(i+pi, j+pj))
-						full = false;
-			if (full)
-				top_ += 1;
-		}
-	
+  int plane;
+  int i, j;
+  bool full;
+  int pi, pj;
+  int vi;
+  
+  // loop over all possible patch centers
+  top_ = 0;
+  for (i=w_; i<im.ni()-w_; i++)
+    for (j=w_; j<im.nj()-w_; j++) {
+      full = true;
+      patch_center_coords_(top_, 0) = i;
+      patch_center_coords_(top_, 1) = j;
+      // loop over all pixels in a patch
+      for (pi=-w_, vi=0; (pi<=w_) && full; pi++)
+	for (pj=-w_; (pj<=w_) && full; pj++, vi++) 
+	  // if pixel is unfilled, the patch is
+	// not completely full so this patch
+	// cannot be used for lookup operations
+	if (uf(i+pi, j+pj))
+	  full = false;
+	if (full)
+	  top_ += 1;
+    }
+    
 }
 
 ///////////////////////////////////////////////////////////
@@ -76,93 +76,96 @@ void patch_db::compute_patch_centers(
 //
 
 bool patch_db::lookup(
-			const vnl_matrix<int>* target_planes, 
-			int nplanes,
-			const vnl_matrix<int>& target_unfilled, 
-			int& source_i, 
-			int& source_j
-			)
+  const vnl_matrix<int>* target_planes, 
+  int nplanes,
+  const vnl_matrix<int>& target_unfilled, 
+  int& source_i, 
+  int& source_j
+)
 {
-	int i, match;
-
-	// if the data structures were not correctly initialized, 
-	// quit the lookup operation
-	if (top_ == 0)
-		return false;
-
-	// if the size of the supplied matrices is NOT equal to the 
-	// patch size used for contstructing the patch_db object,
-	// quit the lookup operation
-	for (i=0; i<nplanes; i++)
-		if ((target_planes[i].rows() != 2*w_+1) ||
-			(target_planes[i].columns() != 2*w_+1) )
-			return false;
-		
-	///////////////////////////////////////////////////////////
+  int i, match;
+  
+  // if the data structures were not correctly initialized, 
+  // quit the lookup operation
+  if (top_ == 0)
+    return false;
+  
+  // if the size of the supplied matrices is NOT equal to the 
+    // patch size used for contstructing the patch_db object,
+    // quit the lookup operation
+    for (i=0; i<nplanes; i++)
+      if ((target_planes[i].rows() != 2*w_+1) ||
+	(target_planes[i].columns() != 2*w_+1) )
+	return false;
+      
+      ///////////////////////////////////////////////////////////
 	//     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
 	///////////////////////////////////////////////////////////
-
-
+	
+	
 	
 	///////////////////////////////////////////////////////////
 	//              PLACE YOUR CODE HERE                     //
 	///////////////////////////////////////////////////////////
-
-    double curlow = (255*255)*nplanes*plen_*plen_;
-    //goes through all full patches
-    for (i=0; i<=top_; i++) {
-        int pi, pj, x, y, p;
-        double sum;
-
-        //gets the top left corner of the patch
-        pi = patch_center_coords_(i, 0) - w_;
-        pj = patch_center_coords_(i, 1) - w_;
-        
-        //goes through all the pixels to calculate the squares of each pixel
-        for (p=0; p<nplanes; p++) {
-            for (x=0; x<plen_; x++) {
-                for (y=0; y<plen_; y++){
-                    if (target_unfilled[x][y]) continue; //skips if the pixel is unfilled
-                    double dif = im_(pi+x, pj+y, p) - target_planes[p][x][y];
-                    sum += dif*dif;
-                }
-            }
-        }
-        //keeps track of the smallest sum of differences as we go down the list
-        if (sum <= curlow){
-            curlow = sum;
-            match = i;
-        }
-    }
-//	match = top_/2;
-
+	
+	double curlow = (255*255)*nplanes*plen_*plen_;
+	vnl_vector<int> target_r, target_b, target_g, unfilled, pixel;
+	vectorize(target_unfilled, unfilled);
+	//goes through all full patches
+	for (i=0; i<=top_; i++) {
+	  //printf("patch %d\n", i);
+	  int pi, pj, x, y, p;
+	  int sum, dif;
+	  vnl_double_2 center(patch_center_coords_(i,0), patch_center_coords_(i, 1));
+	  psi patch = psi(center, w_, im_.ni(), im_.nj());    
+	  sum = 0;
+	  //goes through all the pixels to calculate the squares of each pixel
+	  for (p=0; p<nplanes; p++) {
+	    vnl_matrix<int> pix, val;
+	    patch.get_pixels(vil_plane(vil_view_as_planes(im_), p), pix, val);
+	    vectorize(target_planes[p], target);
+	    //printf("plane %d\n", p);
+	    vectorize(pix, pixel);
+	    for (x=0; x<plen_*plen_; x++){
+	      dif = pixel[x] - target[x]*unfilled[x];
+	      sum += dif*dif;
+	    }
+	  }
+	  //keeps track of the smallest sum of differences as we go down the list
+	  if (sum <= curlow){
+	    curlow = sum;
+	    match = i;
+	  }
+	}
+		//match = top_/2;
+	printf("match %d\n",match);
 	///////////////////////////////////////////////////////////
 	//     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
 	///////////////////////////////////////////////////////////
-
-
+	
+	
 	// get row and column coordinates of patch center
 	source_i = patch_center_coords_(match,0);
 	source_j = patch_center_coords_(match,1);
-
+	
 	return true;
-}
+	}
 
 ///////////////////////////////////////////////////////////
 //     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
 ///////////////////////////////////////////////////////////
 
 void patch_db::vectorize(
-			const vnl_matrix<int>& mat, 
-			vnl_vector<int>& vec)
+  const vnl_matrix<int>& mat, 
+  vnl_vector<int>& vec)
 {
-	int i, j, k; 
-
-	vec.set_size(mat.rows() * mat.columns());
-	for (i=0, k=0; i<mat.rows(); i++)
-		for (j=0; j<mat.columns(); j++, k++)
-			vec(k) = mat(i, j);
-
+  int i, j, k; 
+  
+  vec.set_size(mat.rows() * mat.columns());
+  for (i=0, k=0; i<mat.rows(); i++)
+    for (j=0; j<mat.columns(); j++, k++)
+      vec(k) = mat(i, j);
+    
 }
 
 /////////////////////////////////////////////////////////
