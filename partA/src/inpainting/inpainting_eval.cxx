546e079bf2ee57bdc58c19fc2a29f42e6939dbab
Christy Sham <g2g@cdf.toronto.edu>
2013-02-28 23:57:16 -0500

    compute_gradient tested, gives satisfactory results (?)

--- partA/src/inpainting/inpainting_eval.cxx
+++ partA/src/inpainting/inpainting_eval.cxx
@@ -55,195 +55,206 @@
 //  
 
 double compute_D(psi& PSI, 
-				 const vil_image_view<vxl_byte> im,
-				 const vil_image_view<bool> unfilled, 
-				 const vil_image_view<bool> fill_front, 
-				 double alpha,
-				 vnl_double_2& gradient, 
-				 vnl_double_2& front_normal)
+		 const vil_image_view<vxl_byte> im,
+		 const vil_image_view<bool> unfilled, 
+		 const vil_image_view<bool> fill_front, 
+		 double alpha,
+		 vnl_double_2& gradient, 
+		 vnl_double_2& front_normal)
 {
-
-	// holds the perpendicular to the gradient
-	vnl_double_2 grad_normal;    
-
-	// compute the gradient at a filled pixel in the 
-	// direction of the front normal
-	if (compute_gradient(PSI, im, unfilled, gradient)) {
-
-		grad_normal(0) = -gradient(1);
-		grad_normal(1) = gradient(0);
-
-		// now compute the normal of the fill front
-		if (compute_normal(PSI, fill_front, front_normal)) {
-			double dotp;
-
-			dotp = fabs(dot_product(grad_normal, front_normal))/alpha;
-
-			return dotp;
-		}
-		if (alpha > 0) 
-			return 1/alpha;
-		else {
-			return 0;
-		}
-	} else {
-		// if we cannot compute a normal, the fill boundary consists
-		// of exactly one pixel; the data term in this case is meaningless
-		// so we just return a default value
-		return 0;
-	}
-}
-
-///////////////////////////////////////////////////////////
-//     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
-///////////////////////////////////////////////////////////
-
-
-//
-// YOU NEED TO IMPLEMENT THE ROUTINES BELOW
-//
-
-
-
-double compute_C(psi& PSI, const vil_image_view<double>& C, 
-				 const vil_image_view<bool>& unfilled)
-{
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
-	///////////////////////////////////////////////////////////
-	
-	vnl_matrix<double> c_vals;
-	vnl_matrix<int> valid, unfilled_pixels, unfilled_valid;
-	double sum;
-	
-	//get the values of images
-	PSI.get_pixels(C, c_vals, valid);
-	PSI.get_pixels(unfilled, unfilled_pixels, unfilled_valid);
-	
-	//sets filled pixels to 1, and unfilled to 0
-	unfilled_pixels = 1 - unfilled_pixels;
-	
-	PSI.begin(unfilled_pixels);
-	
-	while(PSI.next(unfilled_pixels)){
-	  int i, j;
-	  PSI.psi_coord(i, j);
-	  sum += c_vals[i][j];
-	}
-	
-	return sum/(PSI.sz()*PSI.sz());
-
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
-	///////////////////////////////////////////////////////////
-}
-
-
-bool compute_normal(psi& PSI,
-					vil_image_view<bool> fill_front, 
-					vnl_double_2& normal)
-{
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
-	///////////////////////////////////////////////////////////
-
-	
-	///////////////////////////////////////////////////////////
-	//              PLACE YOUR CODE HERE                     //
-	///////////////////////////////////////////////////////////
-
-        // dummy routine
-        normal(0) = 1;
-        normal(1) = 0;
-        return true;
-
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
-	///////////////////////////////////////////////////////////
-}
-
-// return the gradient with the strongest magnitude inside the
-// patch of radius w or return false if no gradients can be computed
-bool compute_gradient(psi& PSI,
-					  const vil_image_view<vxl_byte>& inpainted_grayscale, 
-					  const vil_image_view<bool>& unfilled, 
-					  vnl_double_2& grad)
-{
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
-	///////////////////////////////////////////////////////////
-
-	vnl_matrix<int> greys, valid, unfilled_pixels, unfilled_valid;
-	vnl_matrix<int> smask_x(3,3), smask_y(3,3);
-	double mag, largestMag;
-	bool hasValidPatch = false;
-	largestMag = 0;
-	mag =0;
-	
-	//sobel masks
-	smask_x(0,0) = -1; smask_x(1,0) = -2; smask_x(2,0) = -1;
-	smask_x(0,1) = 0; smask_x(1,1) = 0; smask_x(2,1) = 0;
-	smask_x(0,2) = 1; smask_x(1,2) = 2; smask_x(2,2) = 1;
-	
-	smask_y = smask_x.transpose();
-	
-	//setting up the unfilled mask
-	PSI.get_pixels(unfilled, unfilled_pixels, unfilled_valid);
-	unfilled_pixels = 1 - unfilled_pixels;
-	
-	//goes through all the nonfilled pixels contained in the patch passed in
-	PSI.begin(unfilled_pixels);
-	while (PSI.next(unfilled_pixels)) {
-	  //calculate the gradient of each valid 3x3 patch within the patch
-	  int p, i, j;
-	  PSI.image_coord(i, j);
-	  
-	  //creates a psi for the 3x3 patch centered at the current pixel
-	  vnl_double_2 center(i,j);
-	  psi patch(center, 1, inpainted_grayscale.ni(), inpainted_grayscale.nj());
-	  patch.get_pixels(inpainted_grayscale, greys, valid);
-	  
-	  //gets the intensities for the 3x3 patch
-	  vnl_matrix<int> A, A_valid;
-	  patch.get_pixels(inpainted_grayscale, A, A_valid);
-	  
-	  double gx, gy;
-	  int pi, pj;
-	  //goes through each of the pixels in the 3x3 to calculate the gradient
-	  patch.begin();
-	  while(patch.next()) {
-	    patch.image_coord(i, j);
-	    patch.psi_coord(pi, pj);
-	    if (valid[pi][pj] == 0 || unfilled(i,j) == 1) {
-	      //pixels is invalid or is unfilled
-	      gx = 0;
-	      gy = 1;
-	      break;
-	    }
-	    gx += A[pi][pj]*smask_x[pi][pj];
-	    gy += A[pi][pj]*smask_y[pi][pj];
-	  }
-	  
-	  mag = sqrt(pow(gx,2) + pow(gy,2));
-	  
-	  if (mag > largestMag) {
-	    hasValidPatch = true;
-	    largestMag = mag;
-	    grad(0) = gx;
-	    grad(1) = gy;
-	  }
-	}
-	return hasValidPatch;
-
-	///////////////////////////////////////////////////////////
-	//     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
-	///////////////////////////////////////////////////////////
-}
-
-///////////////////////////////////////////////////////////
-//     PLACE ANY ADDITIONAL CODE (IF ANY) HERE           //
-///////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////
-
+  compute_gradient(PSI, im, unfilled, gradient);
+  return gradient.magnitude();
+  /*
+   *	// holds the perpendicular to the gradient
+   *	vnl_double_2 grad_normal;    
+   * 
+   *	// compute the gradient at a filled pixel in the 
+   *	// direction of the front normal
+   *	if (compute_gradient(PSI, im, unfilled, gradient)) {
+   * 
+   *		grad_normal(0) = -gradient(1);
+   *		grad_normal(1) = gradient(0);
+   * 
+   *		// now compute the normal of the fill front
+   *		if (compute_normal(PSI, fill_front, front_normal)) {
+   *			double dotp;
+   * 
+   *			dotp = fabs(dot_product(grad_normal, front_normal))/alpha;
+   * 
+   *			return dotp;
+   }
+   if (alpha > 0) 
+     return 1/alpha;
+   else {
+     return 0;
+   }
+   } else {
+     // if we cannot compute a normal, the fill boundary consists
+     // of exactly one pixel; the data term in this case is meaningless
+     // so we just return a default value
+     return 0;
+   }*/
+   }
+   
+   ///////////////////////////////////////////////////////////
+   //     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
+   ///////////////////////////////////////////////////////////
+   
+   
+   //
+   // YOU NEED TO IMPLEMENT THE ROUTINES BELOW
+   //
+   
+   
+   
+   double compute_C(psi& PSI, const vil_image_view<double>& C, 
+		    const vil_image_view<bool>& unfilled)
+   {
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
+     ///////////////////////////////////////////////////////////
+     return 1;
+     // 	vnl_matrix<double> c_vals;
+     // 	vnl_matrix<int> valid, unfilled_pixels, unfilled_valid;
+     // 	double sum;
+     // 	
+     // 	//get the values of images
+     // 	PSI.get_pixels(C, c_vals, valid);
+     // 	PSI.get_pixels(unfilled, unfilled_pixels, unfilled_valid);
+     // 	
+     // 	//sets filled pixels to 1, and unfilled to 0
+     // 	unfilled_pixels = 1 - unfilled_pixels;
+     // 	
+     // 	PSI.begin(unfilled_pixels);
+     // 	
+     // 	while(PSI.next(unfilled_pixels)){
+       // 	  int i, j;
+     // 	  PSI.psi_coord(i, j);
+     // 	  sum += c_vals[i][j];
+     // 	}
+     // 	
+     // 	return sum/(PSI.sz()*PSI.sz());
+     
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
+     ///////////////////////////////////////////////////////////
+   }
+   
+   
+   bool compute_normal(psi& PSI,
+		       vil_image_view<bool> fill_front, 
+		       vnl_double_2& normal)
+   {
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
+     ///////////////////////////////////////////////////////////
+     
+     
+     ///////////////////////////////////////////////////////////
+     //              PLACE YOUR CODE HERE                     //
+     ///////////////////////////////////////////////////////////
+     
+     // dummy routine
+     normal(0) = 1;
+     normal(1) = 0;
+     return true;
+     
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
+     ///////////////////////////////////////////////////////////
+   }
+   
+   // return the gradient with the strongest magnitude inside the
+   // patch of radius w or return false if no gradients can be computed
+   bool compute_gradient(psi& PSI,
+			 const vil_image_view<vxl_byte>& inpainted_grayscale, 
+			 const vil_image_view<bool>& unfilled, 
+			 vnl_double_2& grad)
+   {
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING ABOVE THIS LINE            //
+     ///////////////////////////////////////////////////////////
+     
+     vnl_matrix<int> greys, valid, unfilled_pixels, unfilled_valid;
+     vnl_matrix<int> smask_x(3,3), smask_y(3,3);
+     double mag, largestMag;
+     bool hasValidPatch = false;
+     largestMag = 0;
+     mag =0;
+     
+     //sobel masks
+     smask_x(0,0) = -1; smask_x(1,0) = -2; smask_x(2,0) = -1;
+     smask_x(0,1) = 0; smask_x(1,1) = 0; smask_x(2,1) = 0;
+     smask_x(0,2) = 1; smask_x(1,2) = 2; smask_x(2,2) = 1;
+     
+     smask_y = smask_x.transpose();
+     
+     //setting up the unfilled mask
+     PSI.get_pixels(unfilled, unfilled_pixels, unfilled_valid);
+     unfilled_pixels = 1 - unfilled_pixels;
+     
+     //goes through all the nonfilled pixels contained in the patch passed in
+     PSI.begin(unfilled_pixels);
+     while (PSI.next(unfilled_pixels)) {
+       //calculate the gradient of each valid 3x3 patch within the patch
+       int p, i, j;
+       PSI.image_coord(i, j);
+       
+       //creates a psi for the 3x3 patch centered at the current pixel
+       vnl_double_2 center(i,j);
+       psi patch(center, 1, inpainted_grayscale.ni(), inpainted_grayscale.nj());
+       patch.get_pixels(inpainted_grayscale, greys, valid);
+       
+       //gets the intensities for the 3x3 patch
+       vnl_matrix<int> A, A_valid;
+       patch.get_pixels(inpainted_grayscale, A, A_valid);
+       
+       double gx, gy;
+       int pi, pj;
+       bool allCalculated = true;
+       gx = 0; gy = 0;
+       //goes through each of the pixels in the 3x3 to calculate the gradient
+       patch.begin();
+       while(patch.next()) {
+	 patch.image_coord(i, j);
+	 patch.psi_coord(pi, pj);
+	 if (valid[pi][pj] == 0 || unfilled(i,j) == 1) {
+	   //pixels is invalid or is unfilled
+	   gx = 0;
+	   gy = 1;
+	   allCalculated = false;
+	   break;
+	   
+	 }
+	 
+	 gx += A(pi,pj)*smask_x(pi, pj);
+	 gy += A(pi,pj)*smask_y(pi,pj);
+       }
+       
+       if (allCalculated) {
+	 hasValidPatch = true;
+	 vnl_double_2 g(gx, gy);
+	 mag = g.magnitude();
+	 if (mag > largestMag) {
+	   largestMag = mag;
+	   grad(0) = gx;
+	   grad(1) = gy;
+	 }
+       }
+     }
+     printf("largestMag: %f\n", largestMag);
+     return hasValidPatch;
+     
+     ///////////////////////////////////////////////////////////
+     //     DO NOT CHANGE ANYTHING BELOW THIS LINE            //
+     ///////////////////////////////////////////////////////////
+   }
+   
+   ///////////////////////////////////////////////////////////
+   //     PLACE ANY ADDITIONAL CODE (IF ANY) HERE           //
+   ///////////////////////////////////////////////////////////
+   
+   
+   /////////////////////////////////////////////////////////
+   
+   
